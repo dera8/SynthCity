2af3cb9b53047b20d3179d73f96d0ac713e3c6e6
Debora <debora.russo8@gmail.com>
2024-02-17 15:47:28 +0100

    Create CreazioneMatriciOD.py

---
+++ CreazioneMatriciOD.py
@@ -0,0 +1,54 @@
+import pandas as pd
+
+# Leggi il file CSV
+data = pd.read_csv("matriceod_persone_viaggi.csv", delimiter=';')
+
+# Converti la colonna NumViaggi da float a interi
+data['NumViaggi'] = data['NumViaggi'].str.replace(',', '.').astype(float).astype(int)
+
+# Mappa i valori di mezzo aggregato secondo le specifiche
+mezzo_mapping = {
+    'AUTO': ['AUTO', 'Altro_mezzo_privato', 'Altro'],
+    'BICI': ['BICI'],
+    'FURGONE': ['FURGONE'],
+    'MOTO': ['MOTO'],
+    'PIEDI': ['PIEDI'],
+    'TPL FERRO': ['TPL FERRO'],
+    'TPL GOMMA': ['TPL GOMMA', 'Altro_mezzo_pubblico']
+}
+
+# Applica la mappatura al DataFrame
+for mezzo_aggregato, tipi_mezzo in mezzo_mapping.items():
+    data.loc[data['MEZZO_aggregato'].isin(tipi_mezzo), 'MEZZO_aggregato'] = mezzo_aggregato
+
+# Definizione delle fasce orarie
+fascia_oraria_mapping = {
+    1: {'from_time': '6.30', 'to_time': '9.00'},
+    2: {'from_time': '17.00', 'to_time': '20.00'},
+    0: {'from_time': '0.00', 'to_time': '6.29'}
+}
+
+# Funzione per ottenere l'intervallo di tempo per una data fascia oraria
+def get_time_interval(fascia):
+    return fascia_oraria_mapping.get(fascia, {'from_time': '0.00', 'to_time': '0.00'})
+
+# Creazione della matrice OD
+def create_od_matrix(df, filename, from_time, to_time):
+    with open(filename, 'w') as file:
+        # Intestazione
+        file.write(f'$OR;D2\n* From-Time To-Time\n{from_time} {to_time}\n*Factor\n1.00\n* some\n* additional\n* comments\n')
+        
+        # Dati della matrice
+        for _, row in df.iterrows():
+            origine = row['ORIG_COD_ZONA']
+            destinazione = row['DEST_COD_ZONA']
+            num_viaggi = row['NumViaggi']
+            file.write(f"{origine:>4} {destinazione:>4} {num_viaggi:>4}\n")
+
+# Raggruppa i dati per fascia oraria e mezzo aggregato
+grouped_data = data.groupby(['FASCIA_ORARIA_VIAGGIO', 'MEZZO_aggregato'])
+
+# Crea la matrice OD per ciascuna combinazione di intervallo di tempo e mezzo aggregato
+for (fascia_oraria, mezzo_aggregato), df in grouped_data:
+    intervallo = get_time_interval(fascia_oraria)
+    create_od_matrix(df, f"matrice_OD_{mezzo_aggregato}_{fascia_oraria}.od", intervallo['from_time'], intervallo['to_time'])
